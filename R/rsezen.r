91a9dde950623b746497baa464ebe95ee464bda2
Ismail SEZEN <sezenismail@gmail.com>
2016-05-06 00:57:09 +0300

    fix for man page

--- R/rsezen.r
+++ R/rsezen.r
@@ -1,10 +1,9 @@
 #' rsezen: A package for processing data.
 #'
 #' The rsezen package provides the functions below:
-#' \code{\link{ibicubic}}.
 #'
 #' @section rsezen functions:
-#' The rwind functions ...
+#' \code{\link{ibicubic}}
 #'
 #' @docType package
 #' @name rsezen
